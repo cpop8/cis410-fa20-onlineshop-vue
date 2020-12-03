d72e4bb2e5d2c45da2b2f9650f89f41f75384c60
cpop8 <cpop8@colostate.edu>
2020-12-02 20:54:02 -0700

    Axios, SignUp

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,13 +1,71 @@
 <template>
     <div>
         <h1>Sign Up</h1>
+        <form>
+            <div class="form-group"><label for="fname-input">First Name</label>
+            <input type="text" id="fname-input" required="required"
+            placeholder="First Name" class="form-control" v-model="nameFirst"></div>
+
+            <div class="form-group"><label for="lname-input">Last Name</label>
+            <input type="text" id="lname-input" required="required"
+            placeholder="Last Name" class="form-control" v-model="nameLast"></div>
+
+            <div class="form-group"><label for="email-input">Email Address</label>
+            <input type="email" id="email-input" required="required"
+            placeholder="Enter Email" class="form-control" v-model="email">
+            <small v-if="dupEmail" class="form-text text-danger">Please choose a different email.</small>
+            </div>
+            
+            <div class="form-group"><label for="password-input">Password</label>
+            <input type="password" id="password-input" placeholder="Password" 
+            required="required" value="asdfasdf" class="form-control" v-model="password"></div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+            <p id="error-signup" class="text-danger">{{errorMessage}}</p>
+        </form>
     </div>
 </template>
 
 
 <script>
+import axios from 'axios';
+
 export default {
-    
+    data(){
+        return {
+            nameFirst:'',
+            nameLast:'',
+            email:'',
+            password:'',
+            errorMessage:'',
+            dupEmail: false
+            }
+        },
+    methods:{
+        onSubmit(){
+
+            const myFormData = {
+                nameFirst: this.nameFirst,
+                nameLast: this.nameLast,
+                email: this.email,
+                password: this.password
+                }
+           // console.log(myFormData)
+
+           axios.post("/contacts", myFormData)
+           .then((myResponse)=>{
+               //console.log(myResponse)
+               this.$router.replace("/login?signupsuccess=true")
+               })
+               .catch((myError)=>{
+                   if(myError.response.status === 409){
+                       this.dupEmail = true
+                       }else{
+                           this.errorMessage = "Cannot sign you up, please try again later."
+                           }
+                })
+         }
+    }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
 import router from './routes.js'
+
+axios.defaults.baserURL = "https://cis410-fa20-oo-api.azurewebsites.net"
 
 new Vue({
   el: '#app',
